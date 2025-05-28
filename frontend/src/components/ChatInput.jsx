13adb4f46c9946579f279d8af95f0087af8dc3fa
Tricia-Siah <tricia.siah.wr@gmail.com>
2025-05-28 15:22:51 +0800

    Update ChatInput.jsx design

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -12,11 +12,11 @@ function ChatInput({ newMessage, isLoading, setNewMessage, submitNewMessage }) {
   }
   
   return(
-    <div className='sticky bottom-0 shrink-0 bg-white py-4'>
-      <div className='p-1.5 bg-primary-blue/35 rounded-3xl z-50 font-mono origin-bottom animate-chat duration-400'>
-        <div className='pr-0.5 bg-white relative shrink-0 rounded-3xl overflow-hidden ring-primary-blue ring-1 focus-within:ring-2 transition-all'>
+    <div className='sticky bottom-0 shrink-0 bg-white dark:bg-gray-800 py-4'>
+      <div className='p-1.5 bg-primary-blue/35 dark:bg-gray-700 rounded-3xl'>
+        <div className='pr-0.5 bg-white dark:bg-gray-900 relative shrink-0 rounded-3xl ring-primary-blue ring-1 focus-within:ring-2'>
           <textarea
-            className='block w-full max-h-[140px] py-2 px-4 pr-11 bg-white rounded-3xl resize-none placeholder:text-primary-blue placeholder:leading-4 placeholder:-translate-y-1 sm:placeholder:leading-normal sm:placeholder:translate-y-0 focus:outline-none'
+            className='block w-full max-h-[140px] py-2 px-4 pr-11 bg-white dark:bg-gray-900 text-black dark:text-white placeholder:text-primary-blue focus:outline-none rounded-3xl resize-none'
             ref={textareaRef}
             rows='1'
             value={newMessage}
