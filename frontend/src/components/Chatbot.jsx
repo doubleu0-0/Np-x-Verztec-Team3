7872a9571f52a8a85c4f27fedd7781be22e111f1
XueCong-NP <150408516+XueCong-NP@users.noreply.github.com>
2025-06-12 14:57:26 +0800

    Fixed half of scrolling

--- frontend/src/components/Chatbot.jsx
+++ frontend/src/components/Chatbot.jsx
@@ -138,10 +138,8 @@ function Chatbot() {
   }
 
   return (
-    <div className="relative flex flex-col flex-1 min-h-0">
-
-      {/* Chat Area */}
-      <div className="flex-1 overflow-y-auto px-4 pt-6">
+    <div className="relative flex flex-col h-full min-h-0">
+      <div className="flex-1 overflow-y-auto px-4 pt-6 min-h-0">
         {messages.length === 0 ? (
           <>
             {/* Centered Verztec logo and title */}
